6e6b8e3a99c0d06a57352b02071525a177385c74
samuel <samuelagm@gmail.com>
2016-11-07 14:53:13 +0100

    moving to mac most issues have been fixed

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,6 +1,5 @@
 import * as observable from "data/observable";
 import * as pages from "ui/page";
-import {HelloWorldModel} from "./main-view-model";
 import {Client} from "nativescript-xmpp-client";
 import {Stanza} from "nativescript-xmpp-client";
 import {IQ} from "nativescript-xmpp-client";
@@ -9,26 +8,43 @@ import {IQ} from "nativescript-xmpp-client";
 export function pageLoaded(args: observable.EventData) {
     // Get the event sender
     let page = <pages.Page>args.object;
-    page.bindingContext = new HelloWorldModel();
+    let model = new observable.Observable();
+    page.bindingContext = model;
+}
+
+function registerClient(serverAddress:string){
     var client1 = new Client({
-        websocket: {url: 'ws://192.168.43.220:5280'},
-        jid: 'client2@192.168.43.220',
+        websocket: {url: `ws://${serverAddress}:5280`},
+        jid: `client2@${serverAddress}`,
         password: 'secret'
     });
 
     client1.on('online', function (data) {
-        console.dir(data);
+        //console.dir(data);
         let iq = new IQ({from: data.jid, type: 'get', id: 'rand'}).c('query', {xmlns: 'jabber:iq:roster'});
-        console.log(iq.root().toString());
+        //console.log(iq.root().toString());
         client1.send(iq);
-        client1.send(new Stanza('message', { to: 'client1@localhost/e35da80401810885bcafbe5bbcafd431' }).c('body').t('HelloWorld'))
+        //client1.send(new Stanza('message', { to: 'client1@localhost/e35da80401810885bcafbe5bbcafd431' }).c('body').t('HelloWorld'))
     });
 
     client1.on('stanza', function (stanza) {
-        console.log(stanza.root().toString());
+        if(stanza.is("iq") ){
+            let jsonStanza = stanza.toJSON();
+            console.dir(jsonStanza.children[0].children[0]);
+        }
+        //console.log(stanza.root().toString());
     });
 
     client1.on('error', function (error) {
-        console.log('client2', error)
+        console.log('client2', error);
+        console.dir(error);
     })
+}
+
+export function connect(args: observable.EventData){
+    let page = <pages.Page>args.object;
+    let address = page.bindingContext.get("serverAddress");
+    console.dir(address);
+    registerClient(address);
+
 }
--- xmpp-client.ios.ts
+++ xmpp-client.ios.ts
@@ -1,5 +1,5 @@
 import {Common} from "./xmpp-client.common";
 export var Client = require("./lib/Client");
-export var Stanza = require('node-xmpp-core').Stanza;
-export var Element = require('node-xmpp-core').Element;
-export var IQ =  require('node-xmpp-core').IQ;
+export var Stanza = require('./lib/deps/node-xmpp-core').Stanza;
+export var Element = require('./lib/deps/node-xmpp-core').Element;
+export var IQ =  require('./lib/deps/node-xmpp-core').IQ;
